861c20d28fb5326bb8bfcde2235175afa880073c
igor.bubnevich <igor.bubnevich@zed.com>
2013-07-31 13:18:15 +0400

    add

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 include 'application/bootstrap.php';
 Core\Application::createWebApplication('application/config/webapplication.php')->run();
-var_dump(A::app()->getModuleManager()->load('Main'));
