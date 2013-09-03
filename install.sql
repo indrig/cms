367e276f54404448a013a10c73141d0daee2c0df
igor.bubnevich <igor.bubnevich@zed.com>
2013-09-03 12:11:39 +0400

    install sql

--- install.sql
+++ install.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Авг 29 2013 г., 16:32
+-- Время создания: Сен 03 2013 г., 12:10
 -- Версия сервера: 5.5.25
--- Версия PHP: 5.3.26
+-- Версия PHP: 5.5.3
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,54 @@ SET time_zone = "+00:00";
 --
 -- База данных: `cms`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `module`
+--
+
+CREATE TABLE IF NOT EXISTS `module` (
+  `name` varchar(32) NOT NULL,
+  `active` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `news_category`
+--
+
+CREATE TABLE IF NOT EXISTS `news_category` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `parent_id` int(10) unsigned DEFAULT NULL,
+  `alias` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Дамп данных таблицы `role`
+--
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'Guest'),
+(2, 'User'),
+(3, 'Moderator'),
+(4, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -47,6 +95,60 @@ INSERT INTO `role_privilege` (`role_id`, `resource`, `privilege`) VALUES
 (3, 'Main', 'debug'),
 (3, 'User', 'management'),
 (3, 'Admin', 'read');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `setting`
+--
+
+CREATE TABLE IF NOT EXISTS `setting` (
+  `module` varchar(32) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `value` text,
+  PRIMARY KEY (`module`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `setting`
+--
+
+INSERT INTO `setting` (`module`, `name`, `value`) VALUES
+('main', 'headTitle', 'Global Dynamics');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(64) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=30 ;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_role`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `user_id` bigint(20) unsigned NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `user_role`
+--
+
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
+(2, 4);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
